7113c201cfde45bc501a1838e101458d6abe0990
1i7 <ii1i7@yandex.ru>
2016-11-22 23:23:06 +0300

    Перенести BabblerDebugPanel в отдельный файл

--- babbler-serial-react/react-app-ui.js
+++ babbler-serial-react/react-app-ui.js
@@ -12,16 +12,11 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 import Divider from 'material-ui/Divider';
 
 import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
-import keycode from 'keycode';
 
 import FontIcon from 'material-ui/FontIcon';
 import {red200, green200} from 'material-ui/styles/colors';
 
 import Subheader from 'material-ui/Subheader';
-
-// Babbler.js
-import BabblerDevice from 'babbler-js';
 
 // виджеты Babbler MaterialUI
 //var BabblerConnectionStatusIcon = require('babbler-js-material-ui').BabblerConnectionStatusIcon;
@@ -33,6 +28,11 @@ import BabblerConnectionStatusIcon from 'babbler-js-material-ui/lib/BabblerConne
 import BabblerConnectionErrorSnackbar from 'babbler-js-material-ui/lib/BabblerConnectionErrorSnackbar';
 import BabblerConnectionPanel from 'babbler-js-material-ui/lib/BabblerConnectionPanel';
 import BabblerDataFlow from 'babbler-js-material-ui/lib/BabblerDataFlow';
+
+import BabblerDebugPanel from './widgets/BabblerDebugPanel';
+
+// Babbler.js
+import BabblerDevice from 'babbler-js';
 
 const btnStyle = {
   margin: 12
@@ -113,148 +113,6 @@ var BabblerLedControlPnl = React.createClass({
       }
 });
 
-// Панель отладки
-var BabblerDebugPnl = React.createClass({
-// http://www.material-ui.com/#/components/raised-button
-// http://www.material-ui.com/#/components/text-field
-// https://github.com/callemall/material-ui/blob/v0.15.4/src/TextField/TextField.js#L367
-// http://www.material-ui.com/#/components/subheader
-
-    getInitialState: function() {
-        return {
-            deviceStatus: this.props.babblerDevice.deviceStatus(),
-            cmdValue: ""
-        };
-    },
-    
-    componentDidMount: function() {
-        // слушаем статус устройства
-        this.deviceStatusListener = function(status) {
-            this.setState({deviceStatus: status});
-        }.bind(this);
-        this.props.babblerDevice.on(BabblerDevice.Event.STATUS, this.deviceStatusListener);
-    },
-    
-    componentWillUnmount: function() {
-        // почистим слушателей
-        this.props.babblerDevice.removeListener(BabblerDevice.Event.STATUS, this.deviceStatusListener);
-    },
-    
-    handleCmdChange: function(event) {
-        this.setState({
-          cmdValue: event.target.value,
-        });
-    },
-    
-    handleCmdKeyDown: function(event) {
-        // https://github.com/callemall/material-ui/blob/v0.15.4/src/TextField/TextField.js#L367
-        //console.log(event.keyCode);
-        //if(event.keyCode === 13) {
-        if(this.state.cmdValue.length > 0 && keycode(event) === 'enter') {
-            this.execCmd();
-        }
-    },
-    
-    render: function() {
-        var connected = this.state.deviceStatus === BabblerDevice.Status.CONNECTED ? true : false;
-        return (
-            <div style={{overflowY: "auto", height: 500}}>
-                <div>
-                    <TextField 
-                        hintText="cmd [params]" 
-                        value={this.state.cmdValue} 
-                        onChange={this.handleCmdChange} 
-                        onKeyDown={this.handleCmdKeyDown} 
-                        disabled={!connected} 
-                        fullWidth={true} />
-                    <RaisedButton 
-                        label="Выполнить" 
-                        onClick={this.execCmd} 
-                        disabled={!connected || this.state.cmdValue.length == 0} 
-                        style={btnStyle} />
-                    <RaisedButton label="ping" onClick={this.cmdPing} disabled={!connected} style={btnStyle} />
-                    <RaisedButton label="help" onClick={this.cmdHelp} disabled={!connected} style={btnStyle} />
-                </div>
-                <Subheader>Команда</Subheader>
-                <div style={{minHeight: 26, fontSize: 24, marginLeft: 45}}>{this.state.currCmd}</div>
-                <Subheader>Ответ</Subheader>
-                <div style={{minHeight: 26, fontSize: 24, marginLeft: 45}}>{this.state.reply}</div>
-                <Subheader>Ошибка</Subheader>
-                <div style={{minHeight: 18, marginLeft: 45, color: red200}}>{this.state.error}</div>
-                <Subheader>Данные</Subheader>
-                <BabblerDataFlow
-                    babblerDevice={this.props.babblerDevice}
-                    maxItems={100}
-                    reverseOrder={true}
-                    timestamp={true}
-                    style={{margin: 20}}/>
-            </div>
-        );
-    },
-    
-    execCmd: function() {
-        var cmdParts = this.state.cmdValue.split(" ");
-        // первый кусок - имя команды
-        var cmd = cmdParts[0];
-        // все остальное - параметры
-        cmdParts.shift();
-        var params = cmdParts;
-        
-        this.props.babblerDevice.sendCmd(cmd, params,
-            // onReply
-            function(cmd, params, reply) {
-                this.setState({reply: reply, error: undefined});
-            }.bind(this),
-            // onError
-            function(cmd, params, err) {
-               this.setState({reply: "-", error: err.message});
-            }.bind(this)
-        );
-        this.setState({
-            cmdValue: "", 
-            currCmd: this.state.cmdValue,
-            reply: "",
-            error: ""
-        });
-    }, 
-    
-    cmdPing: function() {
-          this.props.babblerDevice.sendCmd("ping", [],
-              // onReply
-              function(cmd, params, reply) {
-                  this.setState({reply: reply, error: undefined});
-              }.bind(this),
-              // onError
-              function(cmd, params, err) {
-                 this.setState({reply: "-", error: err.message});
-              }.bind(this)
-          );
-          this.setState({
-              currCmd: "ping",
-              reply: "",
-              error: ""
-          });
-      },
-      
-      cmdHelp: function() {
-          this.props.babblerDevice.sendCmd("help", ["--list"],
-              // onReply
-              function(cmd, params, reply) {
-                  this.setState({reply: reply, error: undefined});
-              }.bind(this),
-              // onError
-              function(cmd, params, err) {
-                 this.setState({reply: "-", error: err.message});
-              }.bind(this)
-          );
-          this.setState({
-              currCmd: "help --list",
-              reply: "",
-              error: ""
-          });
-      }
-});
-
 // Устройство Babbler, подключенное к последовательному порту
 var babblerDevice1 = new BabblerDevice();
 
@@ -278,7 +136,7 @@ ReactDOM.render(
                 <BabblerLedControlPnl babblerDevice={babblerDevice1}/>
             </Tab>
             <Tab label="Отладка" >
-                <BabblerDebugPnl babblerDevice={babblerDevice1}/>
+                <BabblerDebugPanel babblerDevice={babblerDevice1}/>
             </Tab>
             <Tab label="Лог" >
                 <BabblerDataFlow 
---
+++ babbler-serial-react/widgets/BabblerDebugPanel.js
@@ -0,0 +1,164 @@
+// BabblerDebugPanel.js
+
+var React = require('react');
+
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import Subheader from 'material-ui/Subheader';
+
+import {red200, green200} from 'material-ui/styles/colors';
+
+import BabblerDataFlow from 'babbler-js-material-ui/lib/BabblerDataFlow';
+
+import BabblerDevice from 'babbler-js';
+
+const btnStyle = {
+  margin: 12
+};
+
+// Панель отладки
+var BabblerDebugPanel = React.createClass({
+// http://www.material-ui.com/#/components/raised-button
+// http://www.material-ui.com/#/components/text-field
+// https://github.com/callemall/material-ui/blob/v0.15.4/src/TextField/TextField.js#L367
+// http://www.material-ui.com/#/components/subheader
+
+    getInitialState: function() {
+        return {
+            deviceStatus: this.props.babblerDevice.deviceStatus(),
+            cmdValue: ""
+        };
+    },
+    
+    componentDidMount: function() {
+        // слушаем статус устройства
+        this.deviceStatusListener = function(status) {
+            this.setState({deviceStatus: status});
+        }.bind(this);
+        this.props.babblerDevice.on(BabblerDevice.Event.STATUS, this.deviceStatusListener);
+    },
+    
+    componentWillUnmount: function() {
+        // почистим слушателей
+        this.props.babblerDevice.removeListener(BabblerDevice.Event.STATUS, this.deviceStatusListener);
+    },
+    
+    handleCmdChange: function(event) {
+        this.setState({
+          cmdValue: event.target.value,
+        });
+    },
+    
+    handleCmdKeyDown: function(event) {
+        // https://github.com/callemall/material-ui/blob/v0.15.4/src/TextField/TextField.js#L367
+        //console.log(event.keyCode);
+        //if(event.keyCode === 13) {
+        if(this.state.cmdValue.length > 0 && keycode(event) === 'enter') {
+            this.execCmd();
+        }
+    },
+    
+    render: function() {
+        var connected = this.state.deviceStatus === BabblerDevice.Status.CONNECTED ? true : false;
+        return (
+            <div style={{overflowY: "auto", height: 500}}>
+                <div>
+                    <TextField 
+                        hintText="cmd [params]" 
+                        value={this.state.cmdValue} 
+                        onChange={this.handleCmdChange} 
+                        onKeyDown={this.handleCmdKeyDown} 
+                        disabled={!connected} 
+                        fullWidth={true} />
+                    <RaisedButton 
+                        label="Выполнить" 
+                        onClick={this.execCmd} 
+                        disabled={!connected || this.state.cmdValue.length == 0} 
+                        style={btnStyle} />
+                    <RaisedButton label="ping" onClick={this.cmdPing} disabled={!connected} style={btnStyle} />
+                    <RaisedButton label="help" onClick={this.cmdHelp} disabled={!connected} style={btnStyle} />
+                </div>
+                <Subheader>Команда</Subheader>
+                <div style={{minHeight: 26, fontSize: 24, marginLeft: 45}}>{this.state.currCmd}</div>
+                <Subheader>Ответ</Subheader>
+                <div style={{minHeight: 26, fontSize: 24, marginLeft: 45}}>{this.state.reply}</div>
+                <Subheader>Ошибка</Subheader>
+                <div style={{minHeight: 18, marginLeft: 45, color: red200}}>{this.state.error}</div>
+                <Subheader>Данные</Subheader>
+                <BabblerDataFlow
+                    babblerDevice={this.props.babblerDevice}
+                    maxItems={100}
+                    reverseOrder={true}
+                    timestamp={true}
+                    style={{margin: 20}}/>
+            </div>
+        );
+    },
+    
+    execCmd: function() {
+        var cmdParts = this.state.cmdValue.split(" ");
+        // первый кусок - имя команды
+        var cmd = cmdParts[0];
+        // все остальное - параметры
+        cmdParts.shift();
+        var params = cmdParts;
+        
+        this.props.babblerDevice.sendCmd(cmd, params,
+            // onReply
+            function(cmd, params, reply) {
+                this.setState({reply: reply, error: undefined});
+            }.bind(this),
+            // onError
+            function(cmd, params, err) {
+               this.setState({reply: "-", error: err.message});
+            }.bind(this)
+        );
+        this.setState({
+            cmdValue: "", 
+            currCmd: this.state.cmdValue,
+            reply: "",
+            error: ""
+        });
+    }, 
+    
+    cmdPing: function() {
+          this.props.babblerDevice.sendCmd("ping", [],
+              // onReply
+              function(cmd, params, reply) {
+                  this.setState({reply: reply, error: undefined});
+              }.bind(this),
+              // onError
+              function(cmd, params, err) {
+                 this.setState({reply: "-", error: err.message});
+              }.bind(this)
+          );
+          this.setState({
+              currCmd: "ping",
+              reply: "",
+              error: ""
+          });
+      },
+      
+      cmdHelp: function() {
+          this.props.babblerDevice.sendCmd("help", ["--list"],
+              // onReply
+              function(cmd, params, reply) {
+                  this.setState({reply: reply, error: undefined});
+              }.bind(this),
+              // onError
+              function(cmd, params, err) {
+                 this.setState({reply: "-", error: err.message});
+              }.bind(this)
+          );
+          this.setState({
+              currCmd: "help --list",
+              reply: "",
+              error: ""
+          });
+      }
+});
+
+
+// отправляем компонент на публику
+module.exports = BabblerDebugPanel;
+
